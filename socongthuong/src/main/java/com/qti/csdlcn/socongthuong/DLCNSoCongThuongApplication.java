facbe0ccc91e5b264d8d57731642a821b92eb499
buithong <vanthong124@gmail.com>
2018-08-20 10:34:24 +0700

    Service quản lý xăng dầu
    - Đại lý xăng dầu
    - Cửa hàng xăng dầu

---
+++ socongthuong/src/main/java/com/qti/csdlcn/socongthuong/DLCNSoCongThuongApplication.java
@@ -0,0 +1,15 @@
+package com.qti.csdlcn.socongthuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DLCNSoCongThuongApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(DLCNSoCongThuongApplication.class, args);
+
+	}
+
+}
---
+++ socongthuong/src/main/java/com/qti/csdlcn/socongthuong/jwt/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.qti.csdlcn.socongthuong.jwt;
+
+import com.qti.csdlcn.socongthuong.jwt.JWTAuthenticationFilter;
+import com.qti.csdlcn.socongthuong.jwt.JWTLoginFilter;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+* Created by nhs3108 on 29/03/2017.
+*/
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username,password, true as enabled from \"user\" where  username=?")
+                .authoritiesByUsernameQuery("select username, role from \"user\" where username=?");
+//                .passwordEncoder(new BCryptPasswordEncoder(16));
+    }
+}
