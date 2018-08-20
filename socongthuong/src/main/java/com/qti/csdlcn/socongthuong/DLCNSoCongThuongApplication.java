44925792e6ff1b0277957d5270bb841ada1a98f0
buithong <vanthong124@gmail.com>
2018-08-20 14:15:21 +0700

    update

--- socongthuong/src/main/java/com/qti/csdlcn/socongthuong/DLCNSoCongThuongApplication.java
+++ socongthuong/src/main/java/com/qti/csdlcn/socongthuong/DLCNSoCongThuongApplication.java
@@ -8,6 +8,7 @@ public class DLCNSoCongThuongApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Demo github
 		SpringApplication.run(DLCNSoCongThuongApplication.class, args);
 
 	}
